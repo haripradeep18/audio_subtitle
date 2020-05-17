222d7bf533197c3b8bd650bb5b79a0e81db42aa6
Haripradeep <haripradeep18@gmail.com>
2020-05-17 20:22:26 +0530

    added logging

--- app.py
+++ app.py
@@ -7,15 +7,28 @@ from flask import (
     jsonify,
     send_file
 )
-
+import traceback
 import sys, os, re, time, shutil
 from gtts import gTTS
 from pydub import AudioSegment
+from logging import Formatter, FileHandler
+import logging
+
+# Create and configure logger
+logging.basicConfig(filename="audio_process.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-from logging import Formatter, FileHandler
 handler = FileHandler(os.path.join(basedir, 'log.txt'), encoding='utf8')
 handler.setFormatter(
     Formatter("[%(asctime)s] %(levelname)-8s %(message)s", "%Y-%m-%d %H:%M:%S")
@@ -37,65 +50,65 @@ def convert_milliseconds(t):
 
 
 def convert_srt_to_audio(in_file):
-    file = open(in_file, "r", encoding='utf-8')
-    lines = file.read()
-    print(in_file)
-    current_directory = UPLOAD_FOLDER
-    str_fold = os.path.basename(in_file).split('.')[0]
-    str_fold = re.sub('[^a-zA-Z0-9\_\n\.\/]', '', str_fold)
-    str_directory = os.path.join(current_directory, str_fold)
-    if not os.path.exists(str_directory):
-        os.makedirs(str_directory)
+    try:
+        file = open(in_file, "r", encoding='utf-8')
+        lines = file.read()
+        logger.info("convert_srt_to_audio called")
+        logger.info(in_file)
+        str_fold = os.path.basename(in_file).split('.')[0]
+        str_fold = re.sub('[^a-zA-Z0-9\_\n\.\/]', '', str_fold)
+        str_directory = os.path.join(UPLOAD_FOLDER, str_fold)
+        if not os.path.exists(str_directory):
+            os.makedirs(str_directory)
 
-    all_dialogs = lines.strip().split("\n\n")
-    final_song = None
-    privious_time = 0
+        all_dialogs = lines.strip().split("\n\n")
+        final_song = None
+        privious_time = 0
 
-    for dialog in all_dialogs[:2]:
-        each_dialog = dialog.split("\n")
-        count, times, dia_text = "%s" % (each_dialog[0]), each_dialog[1], " ".join(each_dialog[2:])
-        start_time_str, end_time_str = times.split('-->')
-        start_time = convert_milliseconds(start_time_str.split(',')[0].strip())
-        end_time = convert_milliseconds(end_time_str.split(',')[0].strip())
+        for dialog in all_dialogs[:2]:
+            each_dialog = dialog.split("\n")
+            count, times, dia_text = "%s" % (each_dialog[0]), each_dialog[1], " ".join(each_dialog[2:])
+            start_time_str, end_time_str = times.split('-->')
+            start_time = convert_milliseconds(start_time_str.split(',')[0].strip())
+            end_time = convert_milliseconds(end_time_str.split(',')[0].strip())
 
-        speech = gTTS(text=dia_text, lang='ta', slow=False)
-        each_dialog_file = os.path.join(str_directory, "%s.mp3" % (count))
-        out_file = re.sub('[^a-zA-Z0-9\_\n\.\/]', '', each_dialog_file)
-        print(out_file)
-        speech.save(out_file)
-        silent_milli_seconds = start_time - privious_time
-        # create 1 sec of silence audio segment
-        silent_segment = AudioSegment.silent(duration=silent_milli_seconds)  # duration in milliseconds
+            speech = gTTS(text=dia_text, lang='ta', slow=False)
+            each_dialog_file = os.path.join(str_directory, "%s.mp3" % (count))
+            out_file = re.sub('[^a-zA-Z0-9\_\n\.\/]', '', each_dialog_file)
+            print(out_file)
+            speech.save(out_file)
+            silent_milli_seconds = start_time - privious_time
+            # create 1 sec of silence audio segment
+            silent_segment = AudioSegment.silent(duration=silent_milli_seconds)  # duration in milliseconds
 
-        # read wav file to an audio segment
-        dia_file = AudioSegment.from_mp3(out_file)
+            # read wav file to an audio segment
+            dia_file = AudioSegment.from_mp3(out_file)
 
-        # Add above two audio segments
-        if out_file.split('/')[-1] == "1.mp3":
-            final_song = silent_segment + dia_file
-        else:
-            final_song = final_song + silent_segment + dia_file
-        privious_time = end_time
+            # Add above two audio segments
+            if out_file.split('/')[-1] == "1.mp3":
+                final_song = silent_segment + dia_file
+            else:
+                final_song = final_song + silent_segment + dia_file
+            privious_time = end_time
 
-    current_directory = os.getcwd()
-    final_out_file = os.path.join(current_directory, "%s.mp3" % (str_fold))
-    print(final_out_file)
-    print(str_fold)
-    final_song.export(final_out_file, format="mp3")
-    send_file(final_out_file, attachment_filename='python.mp3')
-    print('python.jpg')
-    file.close()
+        final_out_file = os.path.join(UPLOAD_FOLDER, "%s.mp3" % (str_fold))
+        logger.info("final_out_file - %s"%(final_out_file))
+
+        final_song.export(final_out_file, format="mp3")
+
+    except Exception:
+        logger.error(traceback.format_exc())
+    finally:
+        file.close()
     return final_out_file
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
-
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'js_static':
@@ -111,7 +124,6 @@ def dated_url_for(endpoint, **values):
                                      'static/css', filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
-
 
 @app.route('/css/<path:filename>')
 def css_static(filename):
@@ -139,16 +151,20 @@ def upldfile():
     if request.method == 'POST':
         files = request.files['file']
         if files and allowed_file(files.filename):
-            filename = files.filename
-            app.logger.info('FileName: ' + filename)
-            # updir = os.path.join(basedir, 'upload/')
-            updir = UPLOAD_FOLDER
-            saved_file = os.path.join(updir, filename)
-            files.save(saved_file)
-            print("saved file path %s"%(saved_file))
-            final_out_file = convert_srt_to_audio(saved_file)
-            print("download is done")
-            file_size = os.path.getsize(saved_file)
+            try:
+                filename = files.filename
+                app.logger.info('FileName: ' + filename)
+                # updir = os.path.join(basedir, 'upload/')
+                updir = UPLOAD_FOLDER
+                saved_file = os.path.join(updir, filename)
+                files.save(saved_file)
+                logger.info("saved file path - %s" % (saved_file))
+                final_out_file = convert_srt_to_audio(saved_file)
+                logger.info("download is done")
+                file_size = os.path.getsize(saved_file)
+
+            except Exception:
+                logger.error(traceback.format_exc())
             return jsonify(name=filename, size=file_size, downloadpath=final_out_file)
 
 
